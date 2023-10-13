ca68a6f885acf807427babd3eb0bd65848b2c7cc
San Tretiak <san.tretiak@gmail.com>
2023-10-13 18:36:08 +0300

    add folder Lacey

---
+++ Lacey/test_060.py
@@ -0,0 +1,32 @@
+import turtle
+turtle.shape("turtle")
+#turtle.forward(120)
+#turtle.right(36)
+#turtle.forward(44)
+#turtle.left(36)
+#turtle.forward(44)
+
+
+# pentagon
+#for i in range(0,5):
+#    turtle.forward(180)
+#    turtle.right(72)
+
+for i in range(0,10):
+    turtle.right(36)    
+    for i in range(0,5):
+        turtle.forward(100)
+        turtle.right(72)
+        
+
+
+
+turtle.exitonclick()
+
+#for i in range(0,4): 
+#    turtle.forward(97)
+#    turtle.right(90)
+
+
+
+
