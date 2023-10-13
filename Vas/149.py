359eac9a1e17e6c93ede2dbeeb40f77f1a2f9b51
San Tretiak <san.tretiak@gmail.com>
2023-10-13 20:22:35 +0300

    149.py - symbs func

--- Vas/149.py
+++ Vas/149.py
@@ -6,7 +6,16 @@ def show(A):
         for s in a:
             print(s, end=" ")
         print()
-# ---
+# function for creating a nested list of random digits
 def rands(m, n):
     res=[[randint(0,9) for i in range(n)] for j in range(m)]
+    return res
+# function for creating a nested list of letters
+def symbs(m, n):
+    val='A'
+    res=[['' for i in range(n)] for j in range(m)]
+    for i in range(m):
+        for j in range(n):
+            res[i][j]=val
+            val=chr(ord(val)+1)
     return res
