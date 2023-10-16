3786311de6d17b1c2a0fba1eab7c7786ff19dc08
San Tretiak <san.tretiak@gmail.com>
2023-10-16 21:50:50 +0300

    fill_snake - GPT version

--- Vas/e_ch3_04.py
+++ Vas/e_ch3_04.py
@@ -4,13 +4,50 @@
 # затем последний столбец(сверху вниз), последняя строка (справа налево), первый столбец
 # (снизу вверх), вторая строка (слева направо), и так далее.
 def fill(m,n):
-    A = [[i for i in range(m)]  for j in range(n)]
+    A = [[m*(j) + (i+1) for i in range(m)]  for j in range(n)]
     return A
 def show(A):
     for a in A:
         for s in a:
             print(s, end=" ")
-        print()    
-A = fill(4,3)
-show(A)
+        print()  
+def fill_snake_matrix(n):
+    matrix = [[0] * n for _ in range(n)]
+    num = 1
+    top, bottom, left, right = 0, n - 1, 0, n - 1
+
+    while top <= bottom and left <= right:
+        for i in range(left, right + 1):
+            matrix[top][i] = num
+            num += 1
+        top += 1
+
+        for i in range(top, bottom + 1):
+            matrix[i][right] = num
+            num += 1
+        right -= 1
+
+        for i in range(right, left - 1, -1):
+            matrix[bottom][i] = num
+            num += 1
+        bottom -= 1
+
+        for i in range(bottom, top - 1, -1):
+            matrix[i][left] = num
+            num += 1
+        left += 1
+
+    return matrix
+
+def print_matrix(matrix):
+    for row in matrix:
+        print(' '.join(map(str, row)))
+
+n = int(input("Введите размер матрицы: "))
+snake_matrix = fill_snake_matrix(n)
+print_matrix(snake_matrix)
+
+
+# A = fill(4,3)
+# show(A)
 
