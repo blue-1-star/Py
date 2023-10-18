567cb0ec50d981be51ed84e7643a132a77fc11ff
San Tretiak <san.tretiak@gmail.com>
2023-10-18 15:16:27 +0300

    swapping arbitrary list items i and j  in list A function swap any pair in list A

--- Vas/e_ch3_06.py
+++ Vas/e_ch3_06.py
@@ -1,5 +1,7 @@
 # ex 06 ch 03  p 170
 from  random import *
+from  copy import *
+
 seed(17)
 def rands(m, n):
     res=[[randint(0,55) for i in range(n)] for j in range(m)]
@@ -11,9 +13,23 @@ def show(A):
         print() 
 def change(A,i,j):
     A.insert(i, A.pop(j))
+#     
+def change_any_pair(A,i,j):
+    # swapping arbitrary list items i and j
+    A.insert(i, A.pop(j))
+    A.insert(j+1, A.pop(i+1))
+    
 
-A = rands(1,9)
-show(A)
+
+# A = rands(1,9)
+A = [randint(0,55) for i in range(9)]
+# show(A)
+print(A)
+B=deepcopy(A)
 print("--------")
-change(A,2,3)
-show(A)
+change(A,2,8)
+change_any_pair(B,2,8)
+print(A)
+print("------")
+print("B=",B)
+# show(A)
