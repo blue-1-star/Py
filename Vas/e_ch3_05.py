89aa7530f1a233fee65770bbe79a0ded61be4dae
San Tretiak <san.tretiak@gmail.com>
2023-10-18 11:51:14 +0300

    modified: Vas/e_ch3_05.py  - function del_row_col(A,dm,dn)

--- Vas/e_ch3_05.py
+++ Vas/e_ch3_05.py
@@ -9,13 +9,30 @@ def show(A):
         for s in a:
             print(s, end=" ")
         print()    
+def del_row_col(A, dm, dn):
+    #  Delete row = dm
+    A.pop(dm-1)
+    # row dimensionality of the matrix minus 1 -> m-1
+
+    for i in range(m-1):
+        A[i].pop(dn)
+        # a column  dn  in each row is deleted
+        A[i].pop(dn-1)
+
 
 dm, dn = 2, 3   # номера удаляемых строки и столбца
-A=rands(4,3) 
-show(A)
-A.pop(dm-1)
-print("-------")
+m, n = 4,5
+A=rands(m,n) 
 show(A)
 
+
+# A.pop(dm-1)
+# print("-------")
+# show(A)
+# for i in range(m-1):
+#     A[i].pop(dn)
+print("-------")
+del_row_col(A,dm,dn)
+show(A)    
 
 
