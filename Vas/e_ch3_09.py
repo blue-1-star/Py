8976795e419179faa7982adc7b03798f3247ee1e
San Tretiak <san.tretiak@gmail.com>
2023-10-19 18:42:14 +0300

    edit e_ch3_09

--- Vas/e_ch3_09.py
+++ Vas/e_ch3_09.py
@@ -1,5 +1,5 @@
 from  random import *
-N=18
+N=7
 seed(17)
 A = [randint(0,98) for i in range(N)]
 B=[]
@@ -7,9 +7,9 @@ def insert_sum(A):
     for i in range(N-1):      
         s = A[i]+A[i+1]
         B.append(s)
-    for i in range(N-1):
-        k=0
-        A.insert(i+k,B[i])
+    k=0
+    for i in range(N-1):       
+        A.insert(1+i+k,B[i])
         k+=1
     return A
 print(A)
