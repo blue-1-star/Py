007d4c8474c3e96c101fd169cbab26c980389300
San Tretiak <san.tretiak@gmail.com>
2023-10-19 14:14:23 +0300

    edit e_ch3_08  - execution of the sort method and simultaneous printing does not work

--- Vas/e_ch3_08.py
+++ Vas/e_ch3_08.py
@@ -5,10 +5,14 @@ A = [randint(0,98) for i in range(18)]
 print(A)
 A1 = A[1::2]
 A2=A[::2]
+print(A1)
 A1s = sorted(A1)
-A2s = sorted(A2,reverse=True) 
-# print(A1.sort(reverse = True))
-# print(A2.sort())
+A2s = sorted(A2,reverse=True)
+
+A1.sort(reverse = True)
+print("A1: ",A1)
+A2.sort()
+print("A2: ",A2)
 # print(A1s)
 # print(A2s)
 
