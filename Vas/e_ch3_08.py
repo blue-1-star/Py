52273d0bf44970072d9536461dc642edb68645c3
San Tretiak <san.tretiak@gmail.com>
2023-10-19 16:01:40 +0300

    e_ch3_08 - gpt solution one and two

--- Vas/e_ch3_08.py
+++ Vas/e_ch3_08.py
@@ -16,4 +16,17 @@ print("A2: ",A2)
 # print(A1s)
 # print(A2s)
 
+# GPT Solution:
+B = []
+for i in range(min(len(A1), len(A2))):
+    B.append(A1[i])
+    B.append(A2[i])
+# Если A2 длиннее A1, добавьте оставшиеся элементы A2
+B.extend(A2[len(A1):])
+print("B:",B)
 
+# second GPT Solution
+C = [item for pair in zip(A1, A2) for item in pair]
+# Если A2 длиннее A1, добавьте оставшиеся элементы A2
+C.extend(A2[len(A1):])
+print("C:",C)
