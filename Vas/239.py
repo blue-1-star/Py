469f64221e0191be1ae811d1533a2e4833b3fc89
San Tretiak <san.tretiak@gmail.com>
2023-10-22 21:26:47 +0300

    add 239.py

---
+++ Vas/239.py
@@ -0,0 +1,14 @@
+# !!!  Очень полезные команды 
+A=["Alpha","Bravo","Charlie"]
+print(A)
+B = ", ".join(A)
+print("B= ",B)
+C = B.split(", ")
+print("С:",C)
+txt= """Прошли года
+И в свете лет
+Есть мудрость
+А вот счастья нет"""
+print(txt)
+D=txt.splitlines()
+print(D)
