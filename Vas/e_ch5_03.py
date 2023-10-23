96f9853e42c68c90bd4a3490b893ba98d1234407
San Tretiak <san.tretiak@gmail.com>
2023-10-23 22:33:20 +0300

    create function swaocase_m and add swap english ( to russian)

--- Vas/e_ch5_03.py
+++ Vas/e_ch5_03.py
@@ -1,4 +1,5 @@
 txt = "А роза упала на лапу Азора! ё ма Ё ВынЬ да полож Ъ __ ::"
+txt_eng = "It’s very handy to know that the ASCII code for 0 is 48 and that the digits codes are in order."
 delta=ord("а")-ord("А")
 # print(delta)
 # print(ord('а'), ord('А'), ord('я'), ord('Я') )
@@ -26,20 +27,25 @@ def between(A,Z,x):
 # print(between(2,15,18))
 # show_chars(1040,1071)  # russian code
 # show_chars(65,122)     # english code
-new_str =""
-for c in txt:
-    if between(1040, 1071, ord(c)):
-        dlt = delta
-    elif between(1072,1103,ord(c)):
-        dlt = -delta
-    elif ord(c) == 1025: 
-        dlt = 80 
-    elif ord(c) == 1105: 
-        dlt = -80         
-    else:
-        dlt = 0
-    # dlt = m*delta
-    ch = chr(ord(c)+dlt)
-    new_str += ch
-print(new_str)
+def swapcase_m(txt):
+    new_str =""
+    for c in txt:
+        if between(1040, 1071, ord(c)) or between(65,90,ord(c)):
+            dlt = delta
+        elif between(1072,1103,ord(c)) or between(97,122,ord(c)):
+            dlt = -delta
+        elif ord(c) == 1025: 
+            dlt = 80 
+        elif ord(c) == 1105: 
+            dlt = -80         
+        else:
+            dlt = 0
+        # dlt = m*delta
+        ch = chr(ord(c)+dlt)
+        new_str += ch
+    return new_str
+print(txt)    
+print(swapcase_m(txt))
+print(txt_eng)
+print(swapcase_m(txt_eng))
 
