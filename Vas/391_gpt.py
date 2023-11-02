7bb4f53781bf1408172c6addc7d6c7bf57057699
San Tretiak <san.tretiak@gmail.com>
2023-11-02 22:07:56 +0200

    add gpt files 391_gpt.py, 391_gpt1.py

---
+++ Vas/391_gpt.py
@@ -0,0 +1,38 @@
+class MyClass:
+    def show(self):
+        print("borderless object")
+        print("Class", self.__class__.__name__)
+
+def create(fields, vals, name=None):
+    if type(name) != str:
+        name = "MisterX"
+    if type(fields) != list or type(vals) != list:
+        return MyClass
+    else:
+        class FieldObject(MyClass):
+            def __init__(self):
+                k = 0
+                for f in fields:
+                    self.__dict__[f] = vals[k]
+                    k += 1
+            def show(self):
+                print("field object")
+                for s in dir(self):
+                    if not s.startswith("_") and s != "show":
+                        print(s, "=", self.__dict__[s])
+                print("Class", self.__class__.__name__)
+        FieldObject.__name__ = name
+        return FieldObject
+
+A = create(["red", "green", "blue"], [1, 2, 3], "MyColors")
+A.show()
+B = create(["alpha", "bravo"], ["Alpha", "Bravo"])
+B.show()
+C = create(1, 2, 3)
+C.show()
+A.red = 100
+A.green = 200
+A.blue = 300
+A.show()
+D = A.__class__()
+D.show()
