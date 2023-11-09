ea24c1d79223fa1be0fa41b18ea4eaefbcc9f492
San Tretiak <san.tretiak@gmail.com>
2023-11-09 13:48:05 +0200

    edit 468.py   iterator

--- Vas/468.py
+++ Vas/468.py
@@ -29,3 +29,33 @@ try:
         print(next(B), end=" ")
 except StopIteration:
     print()
+# Листинг 9.22. Создание итератора
+class MyClass:
+    def __init__(self,*vals):
+        L=[]
+        for v in vals:
+            if type(v)==int:
+                if v<10 and v>0:
+                    L.append(v)
+        self.digits=L
+        self.position= -1
+    def __iter__(self):
+        return self
+    def __next__(self):
+        self.position+=1
+        if self.position<len(self.digits):
+            return self.digits[self.position]
+        else:
+            raise StopIteration    
+AA=MyClass(2,"A",12,7,-3,"Hello",9,5,"Alpha")  
+try:
+    while True:
+        print(next(AA), end=" ")
+except StopIteration:
+    print()          
+# Create Iterator
+BB=MyClass(5,"B",1.2,11,-1,"Hi",8,4,"Bravo",3)
+print("--------------")
+for s in BB:
+    print(s, end=" ")
+print()
