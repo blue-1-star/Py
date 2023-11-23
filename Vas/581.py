66f53d2ee786e5f43cbd9c2e47b5b4161e26282d
San Tretiak <san.tretiak@gmail.com>
2023-11-23 18:38:19 +0200

    add code to 581.py

--- Vas/581.py
+++ Vas/581.py
@@ -36,4 +36,40 @@ class MyApp:
         # list files with pictures
         self.imgFiles=["exit.png","bold.png","italic.png", "normal.png"]
         # path to files directory
-        self.path="D:\\Books\\Python\\Pictures\\"
+        # self.path="D:\\Books\\Python\\Pictures\\"
+        self.path="G:\\Programming\\Py\\Vas\\picture" 
+        # main font
+        self.font=("Courier New",10,"bold")
+        # Creation Main Window
+        def makeMainWindow(self):
+            self.wnd=Tk()
+            self.wnd.title("Define font")
+            self.wnd.geometry(self.position)
+            self.wnd.resizable(False, False)
+        # Show Main Window
+        def showMainWindow(self):
+            self.wnd.mainloop()
+        # Creation Main Menu
+        def makeMainWindow(self):
+            self.wnd=Tk()
+            self.wnd.title("Define font")
+            self.wnd.geometry(self.position)
+            self.wnd.resizable(False, False)
+        def showMainWindow(self):
+            self.wnd.mainloop()  
+        # Creation Main Menu            
+        def makeMainMenu(self):
+            self.menubar=Menu(self.wnd)                      
+        # Creation items Main Menu
+            self.mFont=Menu(self.wnd, font=self.font, tearoff=0)
+            self.mStyle=Menu(self.wnd, font=self.font, tearoff=0)
+            self.mColor=Menu(self.wnd, font=self.font, tearoff=0)
+            self.mAbout=Menu(self.wnd, font=self.font, tearoff=0)            
+        # Fill menu items
+            self.setMenuFont(self.mFont)
+            self.setMenuStyle(self.mStyle)
+            self.setMenuColor(self.mColor)
+            self.mAbout.add_command(label="About program", command=self.showDialog)
+            self.mAbout.add_separator()
+            
+                                
