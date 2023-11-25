35948346401213f643c7954bc1a9d739c60eb705
San Tretiak <san.tretiak@gmail.com>
2023-11-25 19:19:56 +0200

    edit 552 ( resize images), Born_Wild_01,  cats_embrace_01

--- Vas/e_ch11_01.py
+++ Vas/e_ch11_01.py
@@ -1,3 +1,12 @@
 """
 отображается окно с кнопкой, изображением и текстовой меткой. При нажатии кнопки окно закрывается.
 """
+from tkinter import *
+wnd=Tk()
+wnd.title("Simple window with button")
+wnd.geometry("550x400")
+lbl = Label(wnd,text="Hello!",font=("Arial Bold",20))
+btn = Button(wnd,text="Close",command=wnd.destroy)
+btn.place(x=40,y=140,width=100,height=40)
+lbl.place(x=40,y=30)
+wnd.mainloop()
