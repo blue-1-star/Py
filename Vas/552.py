c4edaeced4eda376137f2247a8943dcdb0116ca5
San Tretiak <san.tretiak@gmail.com>
2023-11-25 19:19:56 +0200

    edit 552 ( resize images), Born_Wild_01,  cats_embrace_01

--- Vas/552.py
+++ Vas/552.py
@@ -7,26 +7,28 @@ def change(evt):
             lbl.configure(image=imgs[k])
             break
 # path="D:\\Books\\Python\\Pictures\\"
-path="D:\\Programming\\Py\\Vas\\picture\\"        
+# path="D:\\Programming\\Py\\Vas\\picture\\"        
+path="G:\\Programming\\Py\\Vas\\picture\\"        
 names=["lion","tiger","cat" ]
 # files=["tiger1.jpg","lion.jpg","koala.jpg"]
-files=["cats_embrace.png","tiger1.png","Born_Wild.png"]
+# cats_embrace_01.png  600x659
+files=["cats_embrace_01.png","tiger1.png","Born_Wild_01.png"]
 wnd=Tk()
 wnd.title("Predators")
-wnd.geometry("660x800")
+wnd.geometry("800x900")
 wnd.resizable(False, False)
 imgs=[PhotoImage(file=path+f) for f in files]
 index=0
 lbl=Label(wnd, image=imgs[index])
 lbl.configure(relief=GROOVE)
-lbl.place(x=10, y=10, width=500, height=500)
+lbl.place(x=10, y=10, width=600, height=659)
 cb=Combobox(wnd, state="readonly")
 cb.configure(values=names)
 cb.current(index)
 cb.configure(font=("Arial",11,"bold"))
 cb.bind("<<ComboboxSelected>>", change)
-cb.place(x=10, y=220, width=200, height=30)
+cb.place(x=10, y=700, width=200, height=30)
 btn=Button(wnd, text="OK")
 btn.configure(command=wnd.destroy)
-btn.place(x=60, y=260, width=100, height=30)
+btn.place(x=260, y=700, width=100, height=30)
 wnd.mainloop()
