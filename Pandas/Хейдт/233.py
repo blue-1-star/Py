6e12d1050b32fb70b3680575fe5b9d18345976ba
San Tretiak <san.tretiak@gmail.com>
2023-12-02 23:43:56 +0200

    add emp_dept_db_shema, emp_dept_db_shema_gpt, add my_dbase

--- Pandas/Хейдт/233.py
+++ Pandas/Хейдт/233.py
@@ -55,11 +55,46 @@ print(pd.concat([df1, df3], axis=1))
 # выполняем внутреннее соединение вместо внешнего
 # результат представлен в виде одной строки
 print(pd.concat([df1, df3], axis=1, join='inner'))
-
+# -------------------------------------------------    239
 # это наши клиенты
 customers = {'CustomerID': [10, 11],
 'Name': ['Mike', 'Marcia'],
 'Address': ['Address for Mike',
 'Address for Marcia']}
 customers = pd.DataFrame(customers)
-customers
+print(customers)
+# это наши заказы, сделанные клиентами,
+# они связаны с клиентами с помощью столбца CustomerID
+orders = {'CustomerID': [10, 11, 10],
+'OrderDate': [date(2014, 12, 1),
+date(2014, 12, 1),
+date(2014, 12, 1)]}
+orders = pd.DataFrame(orders)
+print(orders)
+# выполняем слияние датафреймов customers и orders так, чтобы
+# мы могли отправить товары
+print(customers.merge(orders))
+# ---
+# создаем данные, которые будем использовать в качестве примеров
+# в оставшейся части этого раздела
+left_data = {'key1': ['a', 'b', 'c'],
+'key2': ['x', 'y', 'z'],
+'lval1': [ 0, 1, 2]}
+right_data = {'key1': ['a', 'b', 'c'],
+'key2': ['x', 'a', 'z'],
+'rval1': [ 6, 7, 8 ]}
+left = pd.DataFrame(left_data, index=[0, 1, 2])
+right = pd.DataFrame(right_data, index=[1, 2, 3])
+print(left)
+print(right)
+# демонстрируем слияние, не указывая столбцы, по которым
+# нужно выполнить слияние
+# этот программный код неявно выполняет слияние
+# по всем общим столбцам
+print(left.merge(right))
+# демонстрируем слияние, явно задав столбец,
+# по значениям которого нужно связать
+# объекты DataFrame
+left.merge(right, on='key1')
+
+
