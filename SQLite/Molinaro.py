8fe194ba7caec1249543606a360c7ccd42b30668
San Tretiak <san.tretiak@gmail.com>
2023-12-03 21:22:47 +0200

    edit Molinaro

--- SQLite/Molinaro.py
+++ SQLite/Molinaro.py
@@ -25,9 +25,24 @@ results = cursor.fetchall()
 print(results)
 df_emp = pd.io.sql.read_sql(str_1_03, connection, index_col='empno')
 print(df_emp)  
-str_1_04 = "select ename,deptno,sal from emp"
-df_emp1_04 = pd.io.sql.read_sql(str_1_04, connection)
+str_1_04 = "select empno,ename,deptno,sal from emp"
+df_emp1_04 = pd.io.sql.read_sql(str_1_04, connection, index_col='empno')
 print(df_emp1_04)
+print("attribute access:")
+print(df_emp1_04.ename) # attribute access
+# print("loc properties:")
+print("row extraction by position:")
+print(df_emp1_04.iloc[[0,2]]) #   row extraction by position
+# print(df_emp1_04.loc['job'])
+print("df_emp1_04 index:  ",df_emp1_04.index)
+only_first_three = df_emp1_04[:11]
+# print(f'only_first_three = {only_first_three}') 
+# print("delete rows: 0:12 -> ")
+# df_del = df_emp1_04[:12].drop()
+df_del = df_emp1_04.drop(df_emp1_04.index[:11])
+print("delete rows: 0:12 ->\n",df_del)
+
+# ------------
 str_1_05 ="select sal as salary, comm as commission from emp"
 df_emp1_05 = pd.io.sql.read_sql(str_1_05, connection)
 print(df_emp1_05)
@@ -52,6 +67,16 @@ print(df_emp1_11)
 str_1_12 = "select coalesce(comm,0) from emp"
 df_emp1_12 = pd.io.sql.read_sql(str_1_12, connection)
 print(df_emp1_12)
-
-
+# str_1_13 = "select ename, job, deptno from emp where deptno in (10,20)"
+str_1_13 = "select ename, job, deptno from emp where deptno in (10,20) and (ename like '%I%' or job like '%ER%')"
+df_emp1_13 = pd.io.sql.read_sql(str_1_13, connection)
+print(df_emp1_13)
+str_2_01 = "select ename,job,sal from emp \
+where deptno = 10 \
+order by sal asc"
+df_emp2_01 = pd.io.sql.read_sql(str_2_01, connection)
+print(df_emp2_01)
+str_2_02 = "select empno,deptno,sal,ename,job from emp order by deptno, sal desc"
+df_emp2_02 = pd.io.sql.read_sql(str_2_02, connection)
+print(df_emp2_02)
 
