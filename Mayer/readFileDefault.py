060dda791899ec1966a4eecdcb56d4d51cac272b
San Tretiak <san.tretiak@gmail.com>
2023-12-12 23:17:51 +0200

    modify readFileDefault

--- Mayer/readFileDefault.py
+++ Mayer/readFileDefault.py
@@ -1,13 +1,24 @@
 import os
-cd = os.getcwd()
-print("Current directory:",cd)
-filename = "readFileDefault.py" # этот код
-file_path = os.path.join(cd, filename)
-f = open(file_path)
+
+# Получаем путь к директории, содержащей этот скрипт
+script_directory = os.path.dirname(os.path.abspath(__file__))
+print("Script directory:", script_directory)
+
+filename = "readFileDefault.py"
+file_path = os.path.join(script_directory, filename)
 lines = []
-for line in f:
- lines.append(line.strip())
-print(lines)
+with open(file_path, 'r') as f:
+    for line in f:
+        lines.append(line.strip())
+        print(lines)
+    # Ваш код работы с файлом
+    pass
+
+
+# print("Current directory:",cd)
+# filename = "readFileDefault.py" # этот код
+# file_path = os.path.join(cd, filename)
+# f = open(file_path)
 """
 ['filename = "readFileDefault.py" # этот код',
 '',
