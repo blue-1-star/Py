9b81e70577bc66a942bd5ee686b3be2a9654509e
San Tretiak <san.tretiak@gmail.com>
2023-12-27 12:23:52 +0200

    McKinney -ch02_3 finish

--- McKinney/ch02_3.py
+++ McKinney/ch02_3.py
@@ -21,4 +21,21 @@ val_utf8 = val.encode("utf-8")
 print(val_utf8)
 type(val_utf8)
 print(val_utf8.decode("utf-8"))
-
+# p 59 
+from datetime import datetime, date, time
+dt = datetime(2011, 10, 29, 20, 30, 21)
+print(dt.strftime("%d.%m.%Y %H:%M"))
+print(dt.strftime("%Y-%m-%d %H:%M"))
+datetime.strptime("20091031", "%Y%m%d")
+dt_hour = dt.replace(minute=0, second=0)
+print(dt_hour)
+dt2 = datetime(2011, 11, 15, 22, 30)
+delta = dt2 - dt
+print(delta)
+# p 62 
+sequence = [1, 2, None, 4, None, 5]
+total = 0
+for value in sequence:
+    if value is None:
+        continue
+    total += value
