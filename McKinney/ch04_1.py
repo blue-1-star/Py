96a4b339ecabee36e2c3a47f98042d501801dbb6
San Tretiak <san.tretiak@gmail.com>
2023-12-29 23:06:02 +0200

    modify ch04_1

--- McKinney/ch04_1.py
+++ McKinney/ch04_1.py
@@ -23,6 +23,19 @@ start_time = time.time()
 my_list2 = [x * 2 for x in my_list]
 end_time = time.time()
 elapsed_time = end_time - start_time
-
 print(f"Time taken for list operation: {elapsed_time} seconds")
+
+# p 102
+data1 = [6, 7.5, 8, 0, 1]
+arr1 = np.array(data1)
+print(arr1)
+data2 = [[1, 2, 3, 4], [5, 6, 7, 8]]
+arr2 = np.array(data2)
+# p 109
+arr2d = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(arr2d)
+print(arr2d[2])
+print(arr2d[0][2])
+print(arr2d[0,2])
+
 
