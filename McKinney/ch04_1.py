9df70c624869d0faa29d1497f8513c910c7307cb
San Tretiak <san.tretiak@gmail.com>
2023-12-30 22:23:00 +0200

    modified ch04_1

--- McKinney/ch04_1.py
+++ McKinney/ch04_1.py
@@ -37,5 +37,17 @@ print(arr2d)
 print(arr2d[2])
 print(arr2d[0][2])
 print(arr2d[0,2])
+lower_dim_slice = arr2d[1, :2]
+print(lower_dim_slice)
+print(arr2d[:2, 2])
+#
+# -------------  p 125
+xarr = np.array([1.1, 1.2, 1.3, 1.4, 1.5])
+yarr = np.array([2.1, 2.2, 2.3, 2.4, 2.5])
+cond = np.array([True, False, True, True, False])
+result = [(x if c else y) for x, y, c in zip(xarr, yarr, cond)]
+print(result)
+res_np = np.where(cond, xarr, yarr)
+print(f'NumPy->\n{res_np}')
 
 
