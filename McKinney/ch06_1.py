411191b148701211af9796bfe47b6a3e7d33ad9d
San Tretiak <san.tretiak@gmail.com>
2024-01-05 22:04:13 +0200

    modified ch06_1

--- McKinney/ch06_1.py
+++ McKinney/ch06_1.py
@@ -13,6 +13,41 @@ print(df3)
 parsed = pd.read_csv(ph+"csv_mindex.csv",
 index_col=["key1", "key2"])
 print(parsed)
+result = pd.read_csv(ph+ "ex3.txt", sep="\s+")
+print(result)
+result = pd.read_csv(ph+ "ex4.csv", skiprows=[0,2,3])
+print(f' skiprows->\n{result}')
+result = pd.read_csv(ph + "ex5.csv")
+print(f'ex5.csv  NaN->\n{result}')
+import sys
+data = pd.read_csv(ph + "ex5.csv")
+data.to_csv(sys.stdout, sep="|")
+data.to_csv(sys.stdout, na_rep="NULL")
+# p 196
+import csv
+f = open(ph + "ex7.csv")
+reader = csv.reader(f)
+for line in reader:
+    print(line)
+f.close()    
+with open(ph + "ex7.csv") as f:
+    lines = list(csv.reader(f))
+header, values = lines[0], lines[1:]
+data_dict = {h: v for h, v in zip(header, zip(*values))}
+print(data_dict)
+
+# p 198
+obj = """
+{"name": "Wes",
+"cities_lived": ["Akron", "Nashville", "New York", "San Francisco"],
+"pet": null,
+"siblings": [{"name": "Scott", "age": 34, "hobbies": ["guitars", "soccer"]},
+{"name": "Katie", "age": 42, "hobbies": ["diving", "art"]}]
+}
+"""
+import json
+res = json.loads(obj)
+print(res)
 
 
 
