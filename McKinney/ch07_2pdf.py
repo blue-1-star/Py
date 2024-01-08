a2abee91543657a585f52639f6f26d6838bbea88
San Tretiak <san.tretiak@gmail.com>
2024-01-08 22:31:09 +0200

    tables modify

--- McKinney/ch07_2pdf.py
+++ McKinney/ch07_2pdf.py
@@ -9,8 +9,8 @@ ph = "G:\\Programming\\Py\\McKinney\\datasets\\Dubr\\"
 # Чтение данных из PDF и создание объекта DataFrame
 # pages_of_interest = [6,7]
 # df = read_pdf(file_path, pages='pages_of_interest')
-df = read_pdf(file_path, pages='all')
-print(f'Number of Tables=  {len(df)}')
+# df = read_pdf(file_path, pages='all')
+# print(f'Number of Tables=  {len(df)}')
 # Вывод полученного DataFrame
 # print(f'table 1\n{df[0]}\n------------------------------------\n')
 # print(f'table 2\n{df[1]}\n------------------------------------\n')
@@ -37,5 +37,8 @@ with pd.ExcelWriter(ph+"tables.xlsx", engine='xlsxwriter') as writer:
     tbl_05.to_excel(writer, sheet_name="Sheet5", index = False)
     tbl_06.to_excel(writer, sheet_name="Sheet6", index = False)
 print("All Sheets created!") 
+
+
+
 
 
