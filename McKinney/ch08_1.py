443f38072ce0cb264182f7ee60ca59e2c5603c9d
San Tretiak <san.tretiak@gmail.com>
2024-01-28 19:57:52 +0200

    FTX_email add and ch08_1 edit

--- McKinney/ch08_1.py
+++ McKinney/ch08_1.py
@@ -2,6 +2,7 @@
 # Слияние объектов DataFrame как в базах данных
 import numpy as np 
 import pandas as pd
+# p 260
 df1 = pd.DataFrame({"key": ["b", "b", "a", "c", "a", "a", "b"],
 "data1": pd.Series(range(7), dtype="Int64")})
 df2 = pd.DataFrame({"key": ["a", "b", "d"],
@@ -9,4 +10,36 @@ df2 = pd.DataFrame({"key": ["a", "b", "d"],
 print(df1)
 print(df2)
 print(pd.merge(df1, df2))
+# p 261
+# Обратите внимание, что я не указал, по какому столбцу производить соеди-
+# нение. В таком случае merge использует в качестве ключей столбцы с одинако-
+# выми именами.
+df3 = pd.DataFrame({"lkey": ["b", "b", "a", "c", "a", "a", "b"],
+"data1": pd.Series(range(7), dtype="Int64")})
+df4 = pd.DataFrame({"rkey": ["a", "b", "d"],
+"data2": pd.Series(range(3), dtype="Int64")})
+print(pd.merge(df3, df4, left_on="lkey", right_on="rkey"))
+"""
+По умолчанию функция merge производит внутреннее соединение ('inner');
+в результирующий объект попадают только ключи, присутствующие в обоих
+объектах-аргументах. 
+Альтернативы: 'left', 'right' и 'outer'. В случае внешнего соединения ('outer')
+берется объединение ключей, т. е. получается то же самое, что при совместном
+применении левого и правого соединений:
+"""
+print(pd.merge(df1, df2, how="outer"))
+print(pd.merge(df3, df4, left_on="lkey", right_on="rkey", how="outer"))
+# p 262
+"""
+При соединении типа многие ко многим строится декартово произведение
+соответственных ключей.
+"""
+df1 = pd.DataFrame({"key": ["b", "b", "a", "c", "a", "b"],
+"data1": pd.Series(range(6), dtype="Int64")})
+df2 = pd.DataFrame({"key": ["a", "b", "a", "b", "d"],
+"data2": pd.Series(range(5), dtype="Int64")})
+print(pd.merge(df1, df2, on="key", how="left"))
+print(pd.merge(df1, df2, on="key", how="inner"))
+
+
 
