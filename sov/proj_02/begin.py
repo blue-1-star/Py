5208d5f0008b848b6db2c61b13e6dd41509cb2f4
San Tretiak <san.tretiak@gmail.com>
2024-04-26 23:37:43 +0300

    begin

---
+++ sov/proj_02/begin.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np 
+import pandas as pd
+ph = "McKinney/examples/"
+dir_dat = "G:/Programming/Py/sov/proj_02/"
+file ="data_02.xlsx" 
+df = pd.read_excel(dir_dat+file)
+# print(df)
+
