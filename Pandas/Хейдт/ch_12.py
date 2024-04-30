1e7c23fe7c31e469a070015622be32d7628f066f
San Tretiak <san.tretiak@gmail.com>
2024-04-30 10:52:05 +0300

    ch_12, ch_14, begin edit

--- Pandas/Хейдт/ch_12.py
+++ Pandas/Хейдт/ch_12.py
@@ -4,3 +4,6 @@ import pandas as pd
 import datetime
 from datetime import datetime, date
 import matplotlib.pyplot as plt
+dir_dat = "G:\\Programming\\Py\\Pandas\\Хейдт\\Data\\"
+sensor_data = pd.read_csv(dir_dat + "sensors.csv")
+print(sensor_data)
