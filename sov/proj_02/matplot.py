60d72b7c9668b584792a22cc3fd1098b989ac8a0
San Tretiak <san.tretiak@gmail.com>
2024-05-04 20:36:37 +0300

    matplot

--- sov/proj_02/matplot.py
+++ sov/proj_02/matplot.py
@@ -136,3 +136,15 @@ def f_3_2_1():
     plt.plot(x, y2)
     plt.show()
 
+def f_3_2_1a():
+    # 3.2.1 Инструмент GridSpec  new layout
+    x = [1, 2, 3, 4, 5]
+    y1 = [9, 4, 2, 4, 9]
+    y2 = [1, 7, 6, 3, 5]
+    fg = plt.figure(figsize=(7, 3), constrained_layout=True)
+    gs = gridspec.GridSpec(ncols=2, nrows=1, figure=fg)
+    fig_ax_1 = fg.add_subplot(gs[0, 0])
+    plt.plot(x, y1)
+    fig_ax_2 = fg.add_subplot(gs[0, 1])
+    plt.plot(x, y2)
+    plt.show()
