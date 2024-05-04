668bc99f5f48146f2bb5c70c8d7835f95eef967b
San Tretiak <san.tretiak@gmail.com>
2024-05-04 20:39:26 +0300

    1

--- sov/proj_02/matplot.py
+++ sov/proj_02/matplot.py
@@ -141,10 +141,13 @@ def f_3_2_1a():
     x = [1, 2, 3, 4, 5]
     y1 = [9, 4, 2, 4, 9]
     y2 = [1, 7, 6, 3, 5]
-    fg = plt.figure(figsize=(7, 3), constrained_layout=True)
-    gs = gridspec.GridSpec(ncols=2, nrows=1, figure=fg)
-    fig_ax_1 = fg.add_subplot(gs[0, 0])
-    plt.plot(x, y1)
-    fig_ax_2 = fg.add_subplot(gs[0, 1])
+    y3 = [-7, -4, 2, -4, -7]
+    fg = plt.figure(figsize=(9, 4), constrained_layout=True)
+    gs = fg.add_gridspec(2, 2)
+    fig_ax_1 = fg.add_subplot(gs[0, :])
     plt.plot(x, y2)
+    fig_ax_2 = fg.add_subplot(gs[1, 0])
+    plt.plot(x, y1)
+    fig_ax_3 = fg.add_subplot(gs[1, 1])
+    plt.plot(x, y3)
     plt.show()
