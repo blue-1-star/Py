9d55dbad2dc8bc4bf861c206d81982c17f7388a1
San Tretiak <san.tretiak@gmail.com>
2024-06-03 10:25:39 +0300

    improvement  copy_photos_by_date

--- FileSys/fs_01a_gpt.py
+++ FileSys/fs_01a_gpt.py
@@ -1,62 +1,5 @@
 import os
 import shutil
-from datetime import datetime
-
-def copy_photos_by_date(source_directory, target_directory):
-    if not os.path.exists(target_directory):
-        os.makedirs(target_directory)
-    cnt =0
-    for filename in os.listdir(source_directory):
-        if filename.startswith("IMG_") and filename.endswith((".jpg", ".jpeg", ".png")):
-            # Извлечение даты из имени файла
-            date_str = filename.split('_')[1]
-            try:
-                date = datetime.strptime(date_str, "%Y%m%d")
-            except ValueError:
-                print(f"Ошибка при обработке файла {filename}: некорректный формат даты.")
-                continue
-            
-            # Формирование имени целевой директории
-            target_subdirectory = os.path.join(target_directory, f"{date.year}_{date.month:02d}")
-            if not os.path.exists(target_subdirectory):
-                os.makedirs(target_subdirectory)
-            
-            # Копирование файла
-            source_file = os.path.join(source_directory, filename)
-            destination_file = os.path.join(target_subdirectory, filename)
-            shutil.copy2(source_file, destination_file)
-            cnt += 1
-            # print(f"Файл {filename} скопирован в {destination_file}")
-    print(f"copied {cnt} files" )
-    
-
-# Пример использования
-# source_directory = 'g:/test/'
-# target_directory = 'g:/test2/'
-source_directory = 'g:/photo/oneplus/photo/'
-target_directory = 'e:/photo/oneplus/photo'
-copy_photos_by_date(source_directory, target_directory)
-
-"""
-Функция def copy_photos_by_date(source_directory, target_directory):
-свою задачу выполняет! 
-Хочу сделать ряд усовершенствованй.
-1. В исходном каталоге ещё имеются видеофайлы  с расширением mp4 - включить их тоже в копирование.
-
-2. Для больших каталогов функция работает долго 
-a) сделать какую то простейшую визуализацию процесса обработки - например изменяющаяся цифра - счетчик обработанных файлов, желательно в одном месте экрана ( без громоздкой графики)  а рядом остаток необработанных файлов.
-
-3. создавать какой то лог - файл с протоколом работы 
-a) дата
-b) пути - источник и целевой файл
-c) количество переданных файлов ( фото и видео - раздельно)
-d) осталось необработанных и где то выдать список ( не уверен что в лог - файле, нарушит структуру! - может просто на экран по итогу работы? )
-e) время обработки
-"""
-
-"""
-import os
-import shutil
 import datetime
 import time
 
@@ -135,5 +78,3 @@ def copy_photos_by_date(source_directory, target_directory):
 source_directory = 'path_to_source_directory'
 target_directory = 'path_to_target_directory'
 copy_photos_by_date(source_directory, target_directory)
-
-"""
