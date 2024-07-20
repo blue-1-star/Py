cee807ef9f88b2ff67aa1e1552da0d58f8e43f83
Леонид Глебов <victory__time@outlook.com>
2024-07-20 18:37:30 +0300

    add test_pyxl

---
+++ Tax/test_pyxl.py
@@ -0,0 +1,17 @@
+import openpyxl
+
+# Создание новой рабочей книги
+wb = openpyxl.Workbook()
+
+# Выбор активного листа
+ws = wb.active
+
+# Запись данных в ячейки
+ws['A1'] = 'Привет'
+ws['B1'] = 'Мир'
+
+# Сохранение рабочей книги в файл
+file_path = 'C:/path/to/your/directory/example.xlsx'
+file_path = r'D:\Programming\Py\Tax\data\test.xlsx'
+wb.save(file_path)
+
