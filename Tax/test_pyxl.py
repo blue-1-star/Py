e3e3d145fd0ee9d044ee01a5d302426ed23e6b5e
San Tretiak <san.tretiak@gmail.com>
2024-07-21 21:51:09 +0300

    test_pyxl

--- Tax/test_pyxl.py
+++ Tax/test_pyxl.py
@@ -11,7 +11,7 @@ ws['A1'] = 'Привет'
 ws['B1'] = 'Мир'
 
 # Сохранение рабочей книги в файл
-file_path = 'C:/path/to/your/directory/example.xlsx'
+# file_path = 'C:/path/to/your/directory/example.xlsx'
 file_path = r'D:\Programming\Py\Tax\data\test.xlsx'
 wb.save(file_path)
 
