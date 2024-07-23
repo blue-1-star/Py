ed3f8aaa2206c543038ded4bc90220f700633b55
San Tretiak <san.tretiak@gmail.com>
2024-07-23 17:37:27 +0300

    add multiple string to excel

--- Tax/test_pyxl.py
+++ Tax/test_pyxl.py
@@ -12,12 +12,26 @@ ws['B1'] = 'Мир'
 
 # Сохранение рабочей книги в файл
 # file_path = 'C:/path/to/your/directory/example.xlsx'
-str = 'Всё хорошо прекрасная маркиза! Все хорошо, все хорошо'
-sl =  str.split() 
-print(sl)
-file_path = r'D:\Programming\Py\Tax\data\test.xlsx'
-# wb.save(file_path)
-nr = 3
-fill_row = str.split()
+str1 = 'Всё хорошо прекрасная маркиза! Все хорошо, все хорошо'
+str2 = 'Daily Readings to make your Life the best it can be'
+many_strings = [str1, str2]
+len_ms = len(many_strings) 
+# sl =  str.split() 
+# print(sl)
+file_path = r'G:\Programming\Py\Tax\data\test.xlsx'
+target_column = "A"
+row = 1
+while ws[f"{target_column}{row}"].value:
+    row += 1
+# nr = 3
+# fill_row = str.split()
+for i in range(len_ms):
+    fill_row = many_strings[i].split()
+    for j in range(len(fill_row)):
+        ws.cell(row=row, column=j+1).value = fill_row[j]
+    row += 1
 
+
+# sheet.cell(row=row, column=j).value for j in range(1, len(fill_row)) = fill_row[j] 
+wb.save(file_path)
 
