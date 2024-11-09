e8a4da9ddb5e20050b83c66f1b89f4ff8f6efe5b
San Tretiak <san.tretiak@gmail.com>
2024-11-09 21:55:35 +0200

    mod fs_04b_gpt - verified timestamp for image

--- FileSys/fs_04b_gpt.py
+++ FileSys/fs_04b_gpt.py
@@ -1,5 +1,7 @@
 import subprocess
 from datetime import datetime
+from lib_photos import get_creation_date
+# import get_creation_date from lib_photos 
 import pytz
 
 def get_video_creation_date(video_path):
@@ -29,9 +31,12 @@ video_path1 = 'g:/test/VID_20230527_133445.mp4'
 video_path2 = 'g:/test/VID_20240528_160201.mp4'
 video_path3 = 'G:/Photo/OnePlus/Video/VID_20220719_115940.mp4'
 
+img_path1 = r"G:\test\IMG_20210812_141306.jpg"
+img_path2 = r"G:\test\IMG_20240420_161030.jpg"
+
 # Получение даты создания файлов
 dates = [get_video_creation_date(video_path) for video_path in [video_path1, video_path2, video_path3]]
-
+dates_img = [get_creation_date(img_path) for img_path in [img_path1, img_path2]]
 # Часовой пояс устройства (например, Europe/Kiev)
 device_timezone = pytz.timezone('Europe/Kiev')  # Укажите ваш часовой пояс
 
@@ -41,5 +46,6 @@ converted_dates = [
     for date in dates
 ]
 
-print(f'Оригинальные даты создания видеофайлов (UTC): {dates}')
-print(f'Конвертированные даты создания видеофайлов (местное время): {converted_dates}')
+# print(f'Оригинальные даты создания видеофайлов (UTC): {dates}')
+# print(f'Конвертированные даты создания видеофайлов (местное время): {converted_dates}')
+print(f'Оригинальные даты создания фотофайлов (UTC): {dates_img}')
