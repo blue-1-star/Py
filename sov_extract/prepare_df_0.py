0a5aca198a70cb485f6064a1c14cd07cf9896763
San Tretiak <san.tretiak@gmail.com>
2025-01-20 22:44:33 +0200

    prepare_df_0

---
+++ sov_extract/prepare_df_0.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from datetime import datetime
+from openpyxl import load_workbook
+import tempfile
+
+
+
+
+file = r"G:\My\sov\extract\FL for permuations.xlsx"
+output_dir = os.path.join(os.path.dirname(__file__), 'Data')
