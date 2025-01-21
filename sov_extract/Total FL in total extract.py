fca47c5c3ff5a6696b34e14b0e79b2245b66fb6e
San Tretiak <san.tretiak@gmail.com>
2025-01-21 22:50:42 +0200

    lin_r in prepare_df_0 and plots Differences

--- sov_extract/Total FL in total extract.py
+++ sov_extract/Total FL in total extract.py
@@ -3,9 +3,14 @@ from scipy.stats import shapiro, levene, kruskal
 import scikit_posthocs as sp
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
+
 
 # Step 1: Load the Excel File
-file_path = r'C:\Users\sofii\OneDrive\Рабочий стол\try\Total FL in extract NEW.xlsx'  # Update if file is in another directory
+# file_path = r'C:\Users\sofii\OneDrive\Рабочий стол\try\Total FL in extract NEW.xlsx'  # Update if file is in another directory
+file_path = r"G:\Programming\Py\sov_extract\Data\Total FL in extract NEW.xlsx"
+work_dir = os.path.join(os.path.dirname(__file__), 'Data')
+# work_dir = r"G:\My\sov\extract\Data"
 data = pd.read_excel(file_path)
 
 # Step 2: Rename Columns for Clarity
@@ -65,7 +70,10 @@ if kruskal_test.pvalue < 0.05:
     print(dunn_test_results)
 
     # Save Dunn's Test Results to a CSV File
-    dunn_test_results.to_csv(r"C:\Users\sofii\OneDrive\Рабочий стол\try\dunn_test for total FL_results.csv")
+    # dunn_test_results.to_csv(r"C:\Users\sofii\OneDrive\Рабочий стол\try\dunn_test for total FL_results.csv")
+    output_file = os.path.join(work_dir, f"dunn_test for total FL_results.csv")
+    dunn_test_results.to_csv(output_file)
+
     print("\nDunn's test results saved as 'dunn_test_results.csv'.")
 
     # Heatmap of Dunn's Test Results
@@ -74,7 +82,10 @@ if kruskal_test.pvalue < 0.05:
     sns.heatmap(dunn_test_results, annot=True, fmt=".3f", cmap="coolwarm", cbar=True)
     plt.xlabel("Method", labelpad= 10)
     plt.ylabel("Method", labelpad= 10)
-    plt.savefig(r"C:\Users\sofii\OneDrive\Рабочий стол\try\Heat Map total carb FL.svg")
+    file_name= "Heat Map total carb FL.svg"
+    file_name = os.path.join(work_dir,file_name)
+    plt.savefig(file_name)
+    # plt.savefig(r"C:\Users\sofii\OneDrive\Рабочий стол\try\Heat Map total carb FL.svg")
     #plt.show()
 custom_palette = [
     "#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00", "#ffff33", "#a65628",
@@ -90,5 +101,8 @@ plt.xlabel("Extraction Method", fontsize=14, labelpad= 10)
 plt.ylabel("Total carbohydrate content, mg", fontsize=14, labelpad=10)
 plt.xticks(fontsize=12)
 plt.yticks(fontsize=12)
-plt.savefig(r"C:\Users\sofii\OneDrive\Рабочий стол\try\Total carb_in_FL extract.svg")
+file_name= "Total carb_in_FL extract.svg"
+file_name = os.path.join(work_dir,file_name)
+# plt.savefig(r"C:\Users\sofii\OneDrive\Рабочий стол\try\Total carb_in_FL extract.svg")
+# plt.savefig(r"C:\Users\sofii\OneDrive\Рабочий стол\try\Total carb_in_FL extract.svg")
 #plt.show()
