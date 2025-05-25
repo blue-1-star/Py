8ab8517d5ecbec91d99f11fa90fcc4e7f95c79e4
San Tretiak <san.tretiak@gmail.com>
2025-05-25 14:33:56 +0300

    convert youtube to Yt_dlp_to_mp3_md

--- Video/Yt_dlp_to_mp3_md.py
+++ Video/Yt_dlp_to_mp3_md.py
@@ -2,7 +2,11 @@ import yt_dlp
 from mutagen.easyid3 import EasyID3
 from mutagen.mp3 import MP3
 import os
+import re
 
+def sanitize_filename(name):
+    # Удаление недопустимых символов для имени файла
+    return re.sub(r'[\\/*?:"<>|]', "", name)
 def download_and_convert_to_mp3(url, output_directory='.'):
     try:
         ydl_opts = {
@@ -13,12 +17,14 @@ def download_and_convert_to_mp3(url, output_directory='.'):
                 'preferredcodec': 'mp3',
                 'preferredquality': '360',
             }],
-            'postprocessor_args': ['-metadata', 'title=%(title)s', '-metadata', 'artist=%(uploader)s']
+            'postprocessor_args': ['-metadata', 'title=%(title)s', '-metadata', 'artist=%(uploader)s'],
+            'noplaylist': True  # 👈 Важно: не загружать весь плейлист
         }
         
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(url, download=True)
-            title = info_dict.get('title', 'Unknown Title')
+            # title = info_dict.get('title', 'Unknown Title')
+            title = sanitize_filename(info_dict.get('title', 'Unknown Title'))
             artist = info_dict.get('uploader', 'Unknown Artist')
             album = info_dict.get('album', 'YouTube')
             # year = info_dict.get('year', 'YouTube')
@@ -41,5 +47,7 @@ def download_and_convert_to_mp3(url, output_directory='.'):
 
 # Пример использования
 output_directory = r"G:\Music\fav\2025_spring"
+web_source = "https://www.youtube.com/watch?v=PgXd_93og6o&list=RDGMEMQ1dJ7wXfLlqCjwV0xfSNbA&index=27"
 # download_and_convert_to_mp3('https://www.youtube.com/watch?v=07lcIkYEK-g', output_directory)
-download_and_convert_to_mp3('https://www.youtube.com/watch?v=fN4wqlIh5FE', output_directory)
+download_and_convert_to_mp3(web_source, output_directory)
+
